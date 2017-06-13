bb21ea2e24939c08856d02e4ccd17cfefa8c7096
Arun <arun.salian@gmail.com>
2017-06-13 16:04:27 +0530

    Added Filter

--- src/main/java/com/example/demo/config/AppConfiguration.java
+++ src/main/java/com/example/demo/config/AppConfiguration.java
@@ -4,10 +4,13 @@ import com.example.demo.endpoint.UserService;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.ws.rs.ApplicationPath;
+
 /**
  * Created by c1xadmin on 13-06-2017.
  */
 @Component
+@ApplicationPath("/ws")
 public class AppConfiguration extends ResourceConfig {
     public AppConfiguration () {
         register(UserService.class);
